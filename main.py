c9a6165734ff603561d6c298e237b0c42549f143
Carlos Francisco Pinheiro <carlosfpinheiro@gmail.com>
2018-11-12 02:44:18 -0200

    mudando coisas

--- main.py
+++ main.py
@@ -1,25 +1,43 @@
 # encoding: utf-8
-
-# from collections import namedtuple
-# import sys
-# import csv
-# import random
-# import test_and_training as tat
-
 import sys
 import neural_net as nn
+
+
+def network_config(filename):
+    with open(filename) as file:
+        regularization_parameter = float(next(file))
+        nodes_per_layer = []
+        for line in file:
+            nodes_per_layer.append(int(line))
+        return (regularization_parameter, nodes_per_layer)
+
+
+def network_weights(filename):
+    initial_weights = []
+    with open(filename) as file:
+        for line in file:
+            layer_w = []
+            for substring in line.split(';'):
+                neuron_w = tuple((float(s) for s in substring.split(',')))
+                layer_w.append(neuron_w)
+            initial_weights.append(layer_w)
+    return initial_weights
+
 
 if __name__ == '__main__':
 
     if len(sys.argv) < 4:
-        print('Número de argumentos insuficiente', file=sys.stderr)
+        error_message = 'Modo de usar: \n\tpython3 main.py network.txt initial_weights.txt dataset.txt'
+        print(error_message, file=sys.stderr)
         sys.exit(1)
 
     network_filename = sys.argv[1]
     initial_weights_filename = sys.argv[2]
     dataset_filename = sys.argv[3]
 
-    reg_param, nodes_per_layer = nn.network_config(network_filename)
-    weights = nn.network_weights(initial_weights_filename)
-    print(weights)
+    reg_param, nodes_per_layer = network_config(network_filename)
+    initial_weights = network_weights(initial_weights_filename)
+    print(nodes_per_layer, initial_weights)
 
+    net = nn.Network(nodes_per_layer, initial_weights)
+    print(net.layers)
--- neural_net.py
+++ neural_net.py
@@ -2,28 +2,6 @@ import numpy as np
 import math
 
 
-
-def network_config(filename):
-    with open(filename) as file:
-        regularization_parameter = float(next(file))
-        nodes_per_layer = []
-        for line in file:
-            nodes_per_layer.append(int(line))
-        return (regularization_parameter, nodes_per_layer)
-
-
-def network_weights(filename):
-    w = []
-    with open(filename) as file:
-        for line in file:
-            layer_w = []
-            for substring in line.split(';'):
-                neuron_w = tuple((float(s) for s in substring.split(',')))
-                layer_w.append(neuron_w)
-            w.append(layer_w)
-    return w
-
-
 def sigmoid(x):
     return 1 / (1 + math.exp(-x))
 
@@ -33,3 +11,19 @@ class Neuron:
         self.bias = weights[0]
         self.weights = weights[1:]
 
+    def dot_product(self, values):
+        return np.dot(values, self.weights) + self.bias
+
+    def __repr__(self):
+        return f'Neuron(bias={self.bias}, weights={self.weights}'
+
+
+class Network:
+    def __init__(self, nodes_per_layer, initial_weights):
+        self.initial_layer = nodes_per_layer.pop(0)
+        self.layers = []
+        for weights in initial_weights:
+            layer = []
+            for weight in weights:
+                layer.append(Neuron(weight))
+            self.layers.append(layer)
