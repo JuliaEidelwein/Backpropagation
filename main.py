948ca5d3e523bbe585cc4d6871ecccc15df8afc8
Carlos Francisco Pinheiro <carlosfpinheiro@gmail.com>
2018-11-13 03:25:58 -0200

    Refatorando e removendo código inútil.

--- main.py
+++ main.py
@@ -1,110 +1,35 @@
+#!/usr/bin/env python3
 # encoding: utf-8
-import sys
-import numpy as np
+
 import neural_net as nn
-from collections import namedtuple
+import sys
+import numpy
 
+if len(sys.argv) < 4:
+    error_message = 'Modo de usar: \n\tpython3 main.py network.txt initial_weights.txt dataset.txt'
+    print(error_message, file=sys.stderr)
+    sys.exit(1)
 
+reg_param, nodes_per_layer = nn.network_config(sys.argv[1])
+weights = nn.network_weights(sys.argv[2])
+dataset = nn.parse_instances(sys.argv[3])
+normalized_dataset, max_values, min_values = nn.normalize_dataset(dataset)
 
-# Representa as instâncias lidas do dataset.
-# "data" é a lista de valores de todos os atributos.
-# "result" é a lista de valores experados na saída da rede.
-Instance = namedtuple('Instance', ('data', 'result'))
+print('>> Lambda: ', reg_param, end="\n\n"),
+print('>> Nodos por camada: ', nodes_per_layer, end="\n\n")
+print('>> Pesos iniciais lidos: ', weights, end="\n\n")
+print('>> Dados lidos', dataset, end="\n\n")
+print('>> Dados normalizados', normalized_dataset, end="\n\n")
+print('>> Pesos', weights)
 
+instance = dataset[0]
+z, activations = nn.propagate(weights, instance)
 
-def network_config(filename):
-    '''
-    Dado um nome de arquivo <filename> que representa a configuração da rede,
-    retorna o parâmetro de regularização (lambda), e uma lista contendo o
-    número de neurônios em cada camada.
-    '''
-    with open(filename, 'r') as file:
-        regularization_parameter = float(next(file))
-        nodes_per_layer = []
-        for line in file:
-            nodes_per_layer.append(int(line))
-    return (regularization_parameter, nodes_per_layer)
+print(">> Z: ", z, end="\n\n")
+print(">> Ativações: ", activations, end="\n\n")
 
-
-def network_weights(filename):
-    '''
-    Dado um nome de arquivo <filename> que contém os pesos inicias de cada
-    neurônio, retorna uma lista de tuplas, que contém os pesos iniciais desses
-    neurônios.
-    '''
-    initial_weights = []
-    with open(filename, 'r') as file:
-        for line in file:
-            layer_w = []
-            for substring in line.split(';'):
-                neuron_w = tuple((float(s) for s in substring.split(',')))
-                layer_w.append(neuron_w)
-            initial_weights.append(layer_w)
-    return initial_weights
-
-
-def parse_instances(filename):
-    '''
-    Lê um arquivo e carrega as instâncias de treinamento definidas nele.
-    Retorna uma lista contendo essas isntâncias (que são tuplas).
-    '''
-    dataset = []
-    with open(filename, 'r') as file:
-        next(file)  # Pula a linha com cabeçalhos.
-        for line in file:
-            data, result = [list(map(float, part.split(','))) for part in line.split(';')]
-            dataset.append(Instance(data=data, result=result))
-    return dataset
-
-
-def normalize_dataset(dataset):
-    '''
-    Normalisa cada atributo de cada instância no dataset e retorna uma nova
-    lista de instâncias com valores normalizados entre -1 e 1.
-    '''
-    max_vs = []
-    min_vs = []
-    for index in range(len(dataset[0].data)):
-        values = tuple(instance.data[index] for instance in dataset)
-        max_vs.append(max(values))
-        min_vs.append(min(values))
-
-    normalized_dataset = []
-    for instance in dataset:
-        data = tuple(2 * ((x - min_vs[index]) / (max_vs[index] - min_vs[index])) - 1
-                for index, x in enumerate(instance.data))
-        normalized_dataset.append(Instance(data=data, result=instance.result))
-    return (normalized_dataset, max_vs, min_vs)
-
-
-if __name__ == '__main__':
-
-    if len(sys.argv) < 4:
-        error_message = 'Modo de usar: \n\tpython3 main.py network.txt initial_weights.txt dataset.txt'
-        print(error_message, file=sys.stderr)
-        sys.exit(1)
-
-    network_filename = sys.argv[1]
-    initial_weights_filename = sys.argv[2]
-    dataset_filename = sys.argv[3]
-
-    reg_param, nodes_per_layer = network_config(network_filename)
-    weights = network_weights(initial_weights_filename)
-    dataset = parse_instances(dataset_filename)
-    normalized_dataset, max_values, min_values = normalize_dataset(dataset)
-
-    print('>> Lambda: ', reg_param, end="\n\n"),
-    print('>> Nodos por camada: ', nodes_per_layer, end="\n\n")
-    print('>> Pesos iniciais lidos: ', weights, end="\n\n")
-    print('>> Dados lidos', dataset, end="\n\n")
-    print('>> Dados normalizados', normalized_dataset, end="\n\n")
-
-    layer_weights = nn.initialize_weights_as_matrix(weights, nodes_per_layer)
-
-    n = 0  # Número da saída que estamos analisando
-    z, activations = nn.propagate(layer_weights, dataset[n + 0], nodes_per_layer)
-    errors = nn.J(activations[-1], dataset[n + 0].result)
-    print(f">> Saída predita para o exemplo {n+1}: ", activations[-1], end="\n\n")
-    print(f">> Valor experado para o exemplo {n+1}: ", dataset[n + 0].result, end="\n\n")
-    print(">> Erros obtidos:", errors, end="\n\n")
-    print(nn.sigmoid(np.asarray([1, 2, 3])), end="\n\n")
+errors = nn.J(activations[-1], instance.result)
+print(">> Saída predita para o exemplo:", activations[-1], end="\n\n")
+print(">> Valor experado para o exemplo:", instance.result, end="\n\n")
+print(">> Erros obtidos:", errors, end="\n\n")
+print(nn.sigmoid(numpy.asarray([1, 2, 3])), end="\n\n")
--- neural_net.py
+++ neural_net.py
@@ -1,40 +1,100 @@
+from math import log as ln
+from collections import namedtuple
 import numpy as np
-import math
+
+
+# Representa as instâncias lidas do dataset.
+# "data" é a lista de valores de todos os atributos.
+# "result" é a lista de valores experados na saída da rede.
+class Instance(namedtuple('BasicInstance', ('data', 'result'))):
+    def __repr__(self):
+        data = ', '.join(map(str, self.data))
+        result = ', '.join(map(str, self.result))
+        return f'{{ data: ({data}), result: ({result}) }}'
+
+
+def network_config(filename):
+    '''
+    Dado um nome de arquivo <filename> que representa a configuração da rede,
+    retorna o parâmetro de regularização (lambda), e uma lista contendo o
+    número de neurônios em cada camada.
+    '''
+    with open(filename, 'r') as file:
+        regularization_parameter = float(next(file))
+        nodes_per_layer = []
+        for line in file:
+            nodes_per_layer.append(int(line))
+    return (regularization_parameter, nodes_per_layer)
+
+
+def network_weights(filename):
+    '''
+    Dado um nome de arquivo <filename> que contém os pesos inicias de cada
+    neurônio, retorna uma lista de tuplas, que contém os pesos iniciais desses
+    neurônios.
+
+    Retorna uma lista contendo matrizes de pesos.  Cada matriz corresponde
+    aos pesos de umas das camadas da rede.
+    '''
+    initial_weights = []
+    with open(filename, 'r') as file:
+        for line in file:
+            layer_w = []
+            for substring in line.split(';'):
+                neuron_w = tuple((float(s) for s in substring.split(',')))
+                layer_w.append(neuron_w)
+            initial_weights.append(layer_w)
+    return [np.matrix(w) for w in initial_weights]
+
+
+def parse_instances(filename):
+    '''
+    Lê um arquivo e carrega as instâncias de treinamento definidas nele.
+    Retorna uma lista contendo essas isntâncias (que são tuplas).
+    '''
+    dataset = []
+    with open(filename, 'r') as file:
+        next(file)  # Pula a linha com cabeçalhos.
+        for line in file:
+            data, result = [tuple(map(float, part.split(',')))
+                            for part in line.split(';')]
+            dataset.append(Instance(data=data, result=result))
+    return dataset
+
+
+def normalize_dataset(dataset):
+    '''
+    Normalisa cada atributo de cada instância no dataset e retorna uma nova
+    lista de instâncias com valores normalizados entre -1 e 1.
+    '''
+    max_vs = []
+    min_vs = []
+    for index in range(len(dataset[0].data)):
+        values = tuple(instance.data[index] for instance in dataset)
+        max_vs.append(max(values))
+        min_vs.append(min(values))
+    normalized_dataset = []
+    for instance in dataset:
+        data = tuple(2 * ((x - min_vs[index]) / (max_vs[index] - min_vs[index])) - 1
+                     for index, x in enumerate(instance.data))
+        normalized_dataset.append(Instance(data=data, result=instance.result))
+    return (normalized_dataset, max_vs, min_vs)
 
 
 def sigmoid(fx):
     return 1 / (1 + np.exp(-fx))
 
 
-def create_network(nodes_per_layer, weights):
-    network = []
-    for l in range(1, len(nodes_per_layer)):
-        layer = []
-        for n in range(nodes_per_layer[l]):
-            layer.append(Neuron(weights[l-1][n]))
-        network.append(layer)
-    return network
-
-
-def initialize_weights_as_matrix(weights, nodes_per_layer):
-    layer_weights = []
-    for l in range(1, len(nodes_per_layer)):
-        layer_weights.append(np.matrix(weights[l-1]))
-    return layer_weights
-
-
-def propagate(layer_weights, input, nodes_per_layer):
+def propagate(weights, instance):
     z = []
     activations = []
-    current_activation = np.asarray(input[0])
-    # print(current_activation)
-    for l in range(1, len(nodes_per_layer)):
-        z.append(np.dot(
-            layer_weights[l-1],
-            np.concatenate((np.array([1]), current_activation), axis=None)))
-        current_activation = sigmoid(z[l-1]).A1
+    bias_weight = np.array([1])
+    current_activation = np.asarray(instance.data)
+    for w_i in weights:
+        value = np.dot(w_i, np.concatenate((bias_weight, current_activation)))
+        z.append(value)
+        current_activation = sigmoid(value).A1
         activations.append(current_activation)
-    # print(f'>> Z: {z}\n>> Activations: {activations}')
     return z, activations
 
 
@@ -42,7 +102,7 @@ def calculate_error(y, fx):
     '''
     Calcula o erro entre o valor esperado "y" e o valor obtido "f(x)".
     '''
-    return -y * math.log(fx) - (1 - y) * (math.log(1 - fx))
+    return -y * ln(fx) - (1 - y) * ln(1 - fx)
 
 
 def J(outputs, expected_outputs):
@@ -51,31 +111,3 @@ def J(outputs, expected_outputs):
     Retorna uma lista com os valores dos erros das entradas.
     '''
     return [calculate_error(y, fx) for y, fx in zip(expected_outputs, outputs)]
-
-
-class Neuron:
-    def __init__(self, weights=None, activation=None):
-        if(weights):
-            self.bias = weights[0]
-            self.weights = weights[1:]
-        else:
-            self.bias = None
-            self.weights = None
-        self.activation = activation
-
-    def dot_product(self, values):
-        return np.dot(values, self.weights) + self.bias
-
-    def __repr__(self):
-        return f'Neuron(bias={self.bias}, weights={self.weights}'
-
-
-class Network:
-    def __init__(self, nodes_per_layer, initial_weights):
-        self.initial_layer = nodes_per_layer.pop(0)
-        self.layers = []
-        for weights in initial_weights:
-            layer = []
-            for weight in weights:
-                layer.append(Neuron(weight))
-            self.layers.append(layer)
